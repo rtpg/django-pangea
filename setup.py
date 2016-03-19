5fcdce9529b651b0ffa8f411797bbb7f3b57ca3f
Raphael Gaschignard <rtpg@rtpg.co>
2016-03-19 17:55:30 +0900

    add license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django-pangea',
+    packages=['django-pangea'],
+    version='0.1',
+    license='MIT',
+    description='Django model methods that you can execute everywhere',
+    author='Raphael Gaschigard',
+    author_email='raphael@rtpg.co',
+    url='https://github.com/rtpg/django-pangea',
+    download_url='https://github.com/rtpg/django-pangea/tarball/0.1',
+)
